098463f63f9f23b7bbe4e3da3e9979ac40c10292
sah alom <sahalom4729@gmail.com>
2024-07-12 14:48:16 +0400

    update nav btn

--- src/SharedComponent/Navbar.jsx
+++ src/SharedComponent/Navbar.jsx
@@ -9,10 +9,12 @@ import 'react-tooltip/dist/react-tooltip.css';
 import { Tooltip } from 'react-tooltip';
 import { Link } from "react-scroll";
 import './Navbar.css';
+import { motion } from 'framer-motion';
 
 const Navbar = () => {
     const [visible, setVisible] = useState(true);
     const [visibleName, setVisibleName] = useState(true);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     useEffect(() => {
         let prevSPos = window.pageYOffset;
@@ -49,6 +51,52 @@ const Navbar = () => {
         { to: "project", icon: <FaProjectDiagram />, label: "Project" },
         { to: "contact", icon: <RiContactsBook3Fill />, label: "Contact" },
     ];
+    const variants = {
+        hidden: { opacity: 0, y: -20 },
+        visible: (i) => ({
+            opacity: 1,
+            y: 0,
+            transition: {
+                delay: i * 0.1,
+            },
+        }),
+    };
+
+    const navMenu = navItems.map((item, index) => (
+        <motion.li
+            key={item.to}
+            custom={index}
+            initial="hidden"
+            animate={isMenuOpen ? "visible" : "hidden"}
+            variants={variants}
+            className={`hover:bg-color-p hover:text-black font-semibold text-xl rounded-sm bg-slate-900 transition-colors duration-400 ${activeSection === item.to ? 'bg-color-p' : ''}`}
+        >
+               <li
+            key={item.to}
+            className={`hover:bg-color-p p-0 hover:text-black font-semibold text-xl rounded-sm bg-slate-900 transition-colors duration-400 ${activeSection === item.to ? 'bg-color-p' : ''}`}
+        >
+            <Link
+            className={` rounded-sm bg-slate-900 transition-colors duration-800 `}
+            style={{
+                backgroundColor: activeSection === item.to ? '#aec600' : '',
+                color: activeSection === item.to ? 'black' : '',
+                transition: 'background-color 0.5s, color 0.5s',
+            }}
+                to={item.to}
+                spy={true}
+                smooth={true}
+                duration={500}
+                data-tooltip-id="my-tooltip"
+                data-tooltip-content={item.label}
+                activeClass="active"
+                onSetActive={() => handleSetActive(item.to)}
+            >
+                {item.icon}
+            </Link>
+        </li>
+        </motion.li>
+    ));
+
 
     const nav = navItems.map(item => (
         <li
@@ -80,18 +128,19 @@ const Navbar = () => {
         <div className={`w-full m-auto p-0 z-50 fixed top-20`}>
             <div className="p-0 m-auto flex flex-row lg:flex-col md:flex-col justify-start text-white">
                 <div className="navbar-start flex items-center">
-                    <div className="dropdown fixed top-3 z-50">
-                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                    <div className="dropdown fixed top-3 z-40">
+                        <div tabIndex={0} onClick={() => setIsMenuOpen(!isMenuOpen)} role="button" className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
                             </svg>
                         </div>
-                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-gray-500 rounded-box w-40">
-                            {nav}
+                        <ul tabIndex={0} className={`menu menu-sm dropdown-content mt-0 z-[1] p-2 shadow space-y-2 ${isMenuOpen?'bg-gray-400':''}  rounded-sm w-`}>
+                            {navMenu}
                         </ul>
+                        
                     </div>
                     <Link>
-                        <h1 className={`text-xl text-white font-bold ml-5 z-50 fixed md:left-0 lg:left-0 left-8 ${visibleName ? 'top-5 transition-all' : '-top-20 transition-all'} duration-1000`}>
+                        <h1 className={`text-xl text-white font-bold ml-5 z-50 fixed md:left-8 lg:left-0 left-8 ${visibleName ? 'top-5 transition-all' : '-top-20 transition-all'} duration-1000`}>
                             Shah Alom
                         </h1>
                     </Link>
